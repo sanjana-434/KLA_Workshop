68e461cf9c6dbe4d0c6f0b6d5f55d316dc6dc6c4
Sanjana R <sanjanarajasekarandl@gmail.com>
2023-06-23 11:28:35 +0530

    pixel_wise.py file is updated

--- pixel_wise.py
+++ pixel_wise.py
@@ -28,7 +28,14 @@ def convert_image_to_pixels(image_path, pixel_length):
 
     return pixels
 
-def checkDefect(gradient_images):
+def checkDefectByLaplace(images):
+    gradient_images = []
+    for i in range(0,len(image_path)):
+        gradient_images.append(cv2.Laplacian(images[i], cv2.CV_64F))
+
+    gradient_images = np.array(gradient_images)
+    gradient_images = gradient_images.reshape((5,600,800))
+    print(gradient_images.shape)
     result = []
     print('1 : ',gradient_images[0][0][30])
     print('2 : ',gradient_images[1][0][30])
@@ -45,13 +52,31 @@ def checkDefect(gradient_images):
             for k in value_count.keys():
                 if (len(value_count[k]) == 1):
                     result.append([value_count[k][0],i,j])
-    print(len(result))
+    print('Number of Defects : ',len(result))
+    #print(result)
+
+def checkDefectByGrayscale(gradient_images):
+    result = []
+    print('1 : ',gradient_images[0][0][30])
+    print('2 : ',gradient_images[1][0][30])
+    print('3 : ',gradient_images[2][0][30])
+    print('4 : ',gradient_images[3][0][30])
+    print('5 : ',gradient_images[4][0][30])
+    for i in range(0,gradient_images.shape[1]):
+        for j in range(0,gradient_images.shape[2]):
+            value_count = {}
+            for k in range(0,gradient_images.shape[0]):
+                if (gradient_images[k][i][j] not in value_count):
+                    value_count[gradient_images[k][i][j]] = []
+                value_count[gradient_images[k][i][j]].append(k)
+            for k in value_count.keys():
+                if (len(value_count[k]) == 1):
+                    result.append([value_count[k][0],i,j])
+    print('Number of Defects : ',len(result))
     #print(result)   
 
 
-
-
-def convertToLaplace(image_path):
+def convertImage(image_path):
     images = []
     for i in range(0,len(image_path)):
         images.append(cv2.imread(image_path[i], cv2.IMREAD_GRAYSCALE))
@@ -73,7 +98,8 @@ def convertToLaplace(image_path):
     print(gradient_images.shape)
 
 
-    checkDefect(gradient_images)
+    checkDefectByGrayscale(gradient_images)
+    checkDefectByLaplace(images)
 
 
 
@@ -86,4 +112,4 @@ image5_path = 'wafer_image_5.png'
 
 image_path = list([image1_path,image2_path,image3_path,image4_path,image5_path])
 
-result = convertToLaplace(image_path)
+result = convertImage(image_path)
